a89077680e3d305381cf500efd8d25e2944a560b
Ryeberg <69948631+Ryeberg@users.noreply.github.com>
2024-03-25 17:30:52 -0400

    Update DNS Lookup.py

--- DNS Lookup/DNS Lookup.py
+++ DNS Lookup/DNS Lookup.py
@@ -1,11 +1,26 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
 #--GLOBAL VARIABLES--
+    #checks domain name for common url extensions
+def urlExtension(domainName):
+    pattern = r'\.(com|org|net|edu|gov|mil|int)$'
+    match   = re.search(pattern, domainName, re.IGNORECASE)
+    
+    return match is not None
+
+
     #the link: had to put this here, not a fan
 def domainNameLink():
-    domainName = 'fiu.edu'#input('Enter Domain Name: ')
+    domainName = ''
     
+    while not urlExtension(domainName):
+        domainName = input('Enter Domain Name: ')
+        
+        if not urlExtension(domainName):
+            print('\nNo infomation found.\nPlease use url extensions (.com, .org, etc).\n')
+        
     siteName   = 'https://check-host.net/ip-info?host='
     siteToken  = '&csrf_token=4d2ba08a6cadb5a4bcc845de3d4b9a4ddbeba508'
     
@@ -39,9 +54,6 @@ ip2location_tableContents_text   = []
 geolite2_tableContents_text      = []
 ipinfoio_tableContents_text      = []
 
-    #whois tag
-whoIs = soup.find_all('div',class_='flex')
-
 
 #--METHODS--
     #convert the tag elements into strings
@@ -61,35 +73,6 @@ def checkNoneType(table):
             print('FORMATTING ERROR -> ',table[i])
     print('\n')
 
-
-    #print list with table info
-def printTable(table):
-    tableLength = len(table)    
-    for i in range(tableLength):
-        print(table[i])
-    print('\n')
-
-
-    #print all tables, its in the name
-def printAllTables():
-    print(dbIP_table.div.text.strip())
-    printTable(dbIP_tableContents_text)
-    
-    print(ipgeolocation_table.div.text.strip())
-    printTable(ipgeolocation_tableContents_text)
-    
-    print(ip2location_table.div.text.strip())
-    printTable(ip2location_tableContents_text)
-    
-    print(geolite2_table.div.text.strip())
-    printTable(geolite2_tableContents_text)
-    
-    print(ipinfoio_table.div.text.strip())
-    printTable(ipinfoio_tableContents_text)
-
-
-    #collect WhoIs info from bottom of page
-#def whoIsInfo():
 
 
 #gets the element name only from table
@@ -123,6 +106,36 @@ def getElementName(elementName):
     return elementName
 
 
+    #print the format for printElements()
+def printFormat(elementName,tableName,tableContent):
+    #centering the format
+    longestTableName = 29
+    dashes           = ( longestTableName - len(tableName) ) + 10
+    
+    print(f'%s\n%s{" %s" :->{dashes}}\n' %(elementName,tableName,tableContent))
+
+
+    #format the content for printElements()
+def contentFormat(table,element,elementLength):
+    updatedContent = ''
+    contentLength  = len(table[element].text.strip())
+
+    if (contentLength + 1 == elementLength):
+        return updatedContent
+    else:
+        match element:
+            case 2:     #"3" IP Range
+                updatedContent = table[element].text.strip()[elementLength + 1:-6] + ' ' + table[element].text.strip()[contentLength - 4:]
+            case 5:     #"6" Country
+                updatedContent = table[element].text.strip()[elementLength + 2:-5] + ' ' + table[element].text.strip()[contentLength - 4:]
+            case 8:     #"9" Time Zone
+                updatedContent = table[element].text.strip()[elementLength + 1:]
+            case default:
+                updatedContent = table[element].text.strip()[elementLength:]
+    
+    return updatedContent
+
+
     #num arg, prints specific element from each table
 def printElements(element):
     #organize the table's name *readable*
@@ -135,15 +148,19 @@ def printElements(element):
     #elementName's length for getting the content
     elementLength = len( getElementName(element) ) + 1
     
-    #element name and the info
-    dbIP_Content = dbIP_tableContents[element].text.strip()[elementLength:]
+    #element content
+    dbIP_Content          = contentFormat(dbIP_tableContents,element,elementLength)
+    ipgeolocation_Content = contentFormat(ipgeolocation_tableContents,element,elementLength)
+    ip2location_Content   = contentFormat(ip2location_tableContents,element,elementLength)
+    geolite2_Content      = contentFormat(geolite2_tableContents,element,elementLength)
+    ipinfoio_Content      = contentFormat(ipinfoio_tableContents,element,elementLength)
     
     #print table name and elements related
-    print(f'%s\n%s{"%s" :->10}\n' %(getElementName(element),dbIP_Content,dbIP))
-    print(ipgeolocation_tableContents[element].text.strip())
-    print(ip2location_tableContents[element].text.strip())
-    print(geolite2_tableContents[element].text.strip())
-    print(ipinfoio_tableContents[element].text.strip())
+    printFormat(getElementName(element),dbIP,dbIP_Content)
+    printFormat(getElementName(element),ipgeolocation,ipgeolocation_Content)
+    printFormat(getElementName(element),ip2location,ip2location_Content)
+    printFormat(getElementName(element),geolite2,geolite2_Content)
+    printFormat(getElementName(element),ipinfoio,ipinfoio_Content)
 
 
     #cleaner this way for downstairs
@@ -159,7 +176,7 @@ def listHeaders():
         print(f'%2d. {headerTags[i].text}' %(i+1))
 
 
-#cases to select specific elements
+    #cases to select specific elements
 def selectElement():
     listHeaders()
     choice = input('Select a table element, 1-11 ("q" to exit)\n')
@@ -201,7 +218,83 @@ def selectElement():
         case 'q':
             print()                     #Quit
         case default:
-            print('Enter a number from 1-11\nEnter again')   #default
+            print('Enter a number from 1-11\nEnter again\n\n')   #default
+            selectElement()
+
+
+    #print list with table info
+def printTable(table):
+    tableLength = len(table)    
+    for i in range(tableLength):
+        print(getElementName(i),table[i][len(getElementName(i))+2:])
+    print('\n')
+
+
+    #print all tables, its in the name
+def printAllTables():
+    print(dbIP_table.div.text.strip())
+    printTable(dbIP_tableContents_text)
+    
+    print(ipgeolocation_table.div.text.strip())
+    printTable(ipgeolocation_tableContents_text)
+    
+    print(ip2location_table.div.text.strip())
+    printTable(ip2location_tableContents_text)
+    
+    print(geolite2_table.div.text.strip())
+    printTable(geolite2_tableContents_text)
+    
+    print(ipinfoio_table.div.text.strip())
+    printTable(ipinfoio_tableContents_text)
+
+
+    #loop through table list, return string
+def listToString(table):
+    stringTable = ''
+    tableLength = len(table)
+    
+    for i in range(tableLength):
+        elementName    = getElementName(i)
+        elementContent = table[i][len(getElementName(i)) + 2:]
+        
+        longestElementName = 12
+        dashes             = ( longestElementName - len(elementName) ) + 10
+        
+        stringTable += f'  %s {" %s":->{dashes}}' %(elementName,elementContent) + '\n'
+    
+    stringTable += '\n\n'
+    
+    return stringTable
+
+
+    #pass the text list to a single String
+def passList():
+    fullTable = ''
+    
+    fullTable += dbIP_table.div.text.strip() + '\n'
+    fullTable += listToString(dbIP_tableContents_text)
+    
+    fullTable += ipgeolocation_table.div.text.strip() + '\n'
+    fullTable += listToString(ipgeolocation_tableContents_text)
+    
+    fullTable += ip2location_table.div.text.strip() + '\n'
+    fullTable += listToString(ip2location_tableContents_text)
+    
+    fullTable += geolite2_table.div.text.strip() + '\n'
+    fullTable += listToString(geolite2_tableContents_text)
+    
+    fullTable += ipinfoio_table.div.text.strip() + '\n'
+    fullTable += listToString(ipinfoio_tableContents_text)
+    
+    return fullTable
+
+
+    #create text file and pass table info 
+def writeFile():
+    tables = open("newFile.txt",'w')
+    tables.write(passList())
+    tables.close()
+
 
 
 #--MAIN METHOD--
@@ -215,6 +308,9 @@ def main():
     
     #printAllTables()
     #selectElement()
+    
+    writeFile()
+    selectElement()
     
 
 #call main method
